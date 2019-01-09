a0f65c680fb4920c26a9b3a689ffbe821e44a064
Julien Girard <girard.julien@gmail.com>
2019-01-09 13:15:55 +0100

    Add legacy scripts

---
+++ renamePhoto.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+EXPECTED_ARGS=2
+
+function displayHelp
+{
+	echo "Usage : `basename $0` [name] [file(s)]"
+	echo "Exemple : ./renamePhoto.sh Julien '/media/DATA/Photos/2012-04_Londres/*.JPG'" 
+	exit 0
+}
+
+function interactiveScript
+{
+	read -p "Suffixe à appliquer aux fichiers : "  suffix
+
+	defaultPath="$PWD/*.JPG"
+	read -p "Liste des photos à modifier [$defaultPath]: " path
+	path="${path:-$defaultPath}"
+
+	rename "$suffix" "$path"
+
+}
+
+function rename
+{
+	exiv2 -F -r"%Y-%m-%d_%H-%M-%S_$1" rename $2
+}
+
+if [ $# -ne $EXPECTED_ARGS ] ; 
+then
+	interactiveScript
+else
+	rename $1 $2
+fi
+
+exit 0
